76b3590402a9956a35f747cf1e9541d398ec7b14
Loren Glenn <lorencglenn@gmail.com>
2017-01-06 09:45:37 -0800

    begin work on api integration

--- tmp/allConcat.js
+++ tmp/allConcat.js
@@ -12,6 +12,24 @@ $(document).ready(function() {
   });
 });
 
+var apiKey = "YOUR-API-KEY-GOES-HERE";
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+     .then(function(response){
+        $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+     })
+
+     .fail(function(error) {
+        $('.showWeather').text(error.responseJSON.message);
+      });
+  });
+});
+
 $(document).ready(function(){
   $('#signup').submit(function(event){
     event.preventDefault();
