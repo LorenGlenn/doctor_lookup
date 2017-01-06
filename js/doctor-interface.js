9a051b3fa177b0b1e3018ad74d08a767246fa4e1
Loren Glenn <lorencglenn@gmail.com>
2017-01-06 11:30:57 -0800

    small fixes to formatting

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,13 +1,11 @@
-var Calculator = require('./../js/doctor.js').calculatorModule;
+var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
-  $('#ping-pong-form').submit(function(event) {
-    event.preventDefault();
-    var goal = $('#goal').val();
-    var simpleCalculator = new Calculator("hot pink");
-    var output = simpleCalculator.pingPong(goal);
-    output.forEach(function(element) {
-      $('#solution').append("<li>" + element + "</li>");
-    });
+  var foundDoctors = new Doctor();
+  $('#doctorLocation').click(function() {
+    var issue = $('#issue').val();
+    $('#issue').val("");
+    $('#solution').empty();
+    foundDoctors.getDoctors(issue);
   });
 });
