f2a6b48dd9b810a17a8873c4896717f8c733aa18
Loren Glenn <lorencglenn@gmail.com>
2017-01-06 10:44:23 -0800

    add input on index page

--- js/doctor.js
+++ js/doctor.js
@@ -1,21 +1,16 @@
-function Calculator(skinName) {
-  this.skin = skinName;
+var apiKey = require('./../.env').apiKey;
+
+function Doctor(){
 }
 
-Calculator.prototype.pingPong = function(goal) {
-  var output = [];
-  for (var i = 1; i <= goal; i++) {
-    if (i % 15 === 0) {
-      output.push("ping-pong");
-    } else if (i % 3 === 0) {
-      output.push("ping");
-    } else if (i % 5 === 0) {
-      output.push("pong");
-    } else  {
-      output.push(i);
-    }
-  }
-  return output;
+Doctor.prototype.getDoctors = function(medicalIssue) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue+'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+   .then(function(result) {
+      console.log(result);
+    })
+   .fail(function(error){
+      console.log("fail");
+    });
 };
 
-exports.calculatorModule = Calculator;
+exports.doctorModule = Doctor;
--- js/doctors-interface.js
+++ js/doctors-interface.js
@@ -1,10 +1,13 @@
-var Doctor = require('./../js/doctors.js').doctorModule;
+var Doctor = require('./../js/doctor.js').doctorModule;
 
 $(document).ready(function() {
   var foundDoctors = new Doctor();
   $('#doctorLocation').click(function() {
     var issue = $('#issue').val();
     $('#issue').val("");
-    foundDoctors.getDoctors(issue);
+    var output = foundDoctors.getDoctors(issue);
+    output.forEach(function(doctor) {
+      $('#solution').append("<li>" + doctor + "</li>");
+    });
   });
 });
